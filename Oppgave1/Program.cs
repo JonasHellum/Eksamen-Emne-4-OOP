a97f0a5565a7ed5c259fc1a31248afff1e7c5644
Jonas Lærke Hellum <jonas_laerke@hotmail.com>
2024-05-06 15:56:04 +0200

    init

---
+++ Oppgave1/Program.cs
@@ -0,0 +1,26 @@
+﻿// Tips: Legg gjerne funksjoner som brukes nederst i denne filen!
+
+// Oppgave 1A
+Console.WriteLine("Oppgave 1A");
+// ... oppgave A her 
+
+
+// Oppgave 1B
+Console.WriteLine("\nOppgave 1B");
+// ... oppgave B her 
+
+
+Console.WriteLine("\nOppgave 1C");
+// ... oppgave C her 
+
+
+Console.WriteLine("\nOppgave 1D");
+// ... oppgave D her 
+
+
+Console.WriteLine("\nOppgave 1E");
+// ... oppgave E her 
+
+
+// Funksjoner kan defineres her 
+// ... funksjoner ...
---
+++ Oppgave5/Program.cs
@@ -0,0 +1,4 @@
+﻿
+// Filen ligger i prosjektet og blir automatisk kopiert til output slik at den kan brukes bare på filnavn!
+string fileName = "cars.csv";
+
